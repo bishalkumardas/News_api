d34d8fc6f538b59a41054aafd73c6b776b53e8e3
Bishal Kumar Das <bishalkumardasofficial11@gmail.com>
2025-04-12 00:10:53 +0530

    change in requirement file

--- api_django/settings.py
+++ api_django/settings.py
@@ -106,11 +106,8 @@ WSGI_APPLICATION = 'api_django.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
-
 
 
 
